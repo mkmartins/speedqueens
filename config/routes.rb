3e7156c8978c572b5c42de40d3ed188adb0ac4f5
Prelang Builder <builder@prelang.com>
2014-05-19 06:14:57 +0000

    Set up member route for #vote action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Speedqueens::Application.routes.draw do
-  resources :home_teachers
+  resources :home_teachers do
+  #->Prelang (voting/acts_as_votable)
+  member do
+    get "vote"
+  end
+end
+
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
   devise_for :admin_users, ActiveAdmin::Devise.config
